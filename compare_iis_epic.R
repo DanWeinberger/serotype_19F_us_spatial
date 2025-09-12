9784bc6277d7dfec77f240cf01c26a781807fd45
Weinberger <daniel.weinberger@yale.edu>
2025-09-12 16:06:38 -0400

    more comparisons of IIS vs epic

--- compare_iis_epic.R
+++ compare_iis_epic.R
@@ -11,27 +11,83 @@ cw <- fips_codes %>%
          fips=paste0(state_code,county_code),
          county_name = gsub(' ','',county_name)
   ) %>%
-  filter(state=='CO') %>%
-  dplyr::select( fips,county)%>%
+  dplyr::select( state,fips,county)%>%
   mutate(county=trimws(county, which = "right"))
+
+#############################
+cw_co <-  cw %>% filter(state=='CO') 
 
 colorado_iis <- read_csv('./Data/colorado_immunization_county/combined_CO_Child_Rates.csv') %>%
     mutate(county=toupper(County)) %>%
-    full_join(cw, by='county') %>%
-  mutate(fips=as.numeric(fips)) %>%
+    full_join(cw_co, by='county') %>%
+  mutate(fips=as.numeric(fips),
+        time = as.Date(sub(" -.*", "", Period),format = "%B %d, %Y") 
+        )%>%
   rename(pct_pcv_iis = 'Up-To-Date Percent') %>%
-  dplyr::select(county, Period, Vaccine,pct_pcv_iis, fips)
+  rename(vaccine=Vaccine) %>%
+  dplyr::select(county,time,  vaccine,pct_pcv_iis, fips)%>%
+  mutate(state = 'CO')
   
+########################################
+
+oregon_files <- list.files('./Data/oregon_immunization_county/staging', full.names = T)
+oregon_iis_list <- lapply(oregon_files, function(X) readxl::read_excel(X))
+
+cw_or <-  cw %>% filter(state=='OR') 
+
+oregon_iis <- bind_rows(oregon_iis_list) %>%
+  rename(Vaccine="...1",
+         county=County) %>%
+  pivot_longer(-c(Vaccine, county)) %>%
+  filter(Vaccine=='4 doses PCV') %>%
+  mutate(county= toupper(county),
+         time = as.Date(paste(name, '01','01', sep='-'))
+         )%>%
+  full_join(cw_or, by='county') %>%
+  rename(vaccine=Vaccine,
+         pct_pcv_iis = value) %>%
+  dplyr::select(county, time, vaccine, pct_pcv_iis, fips) %>%
+  mutate(fips = as.numeric(fips),
+         pct_pcv_iis =100 * pct_pcv_iis)%>%
+  mutate(state = 'OR')
+
+
+###############################
+
+cw_mn <-  cw %>% filter(state=='MN') 
+
+mn_iis <- read_csv('./Data/Minnesota immunizations.csv') %>%
+  filter(vaccine=='PCV')%>%
+  rename(county=location,
+         pct_pcv_iis=percent) %>%
+  mutate(county= toupper(county),
+      time = as.Date(paste(year, '01','01', sep='-')),
+      vaccine = '4 doses PCV'
+    ) %>%
+  dplyr::select(time, fips,vaccine, county, pct_pcv_iis,population) %>%
+  mutate(state = 'MN')
+  
+
+#############
+    
+iis_combined <- bind_rows(mn_iis, colorado_iis, oregon_iis)
+
+
+#Limited to base patient, has any encounters, has an immunization registry query
 epic <- read.csv('./Data/abcs_epic_pcv_uptake.csv') %>%
-  filter(age=='age_1_2y' & grepl(',CO',county_name)) %>%
-  rename(pct_pcv_epic = pct_pcv)
+  filter(age=='age_1_2y' ) %>%
+  rename(pct_pcv_epic = pct_pcv) %>%
+  mutate(state =sub(".*,", "", county_name))
 
 compare <- epic %>%
-  full_join(colorado_iis, by='fips') %>%
-  filter(Period=='January 1, 2019 - June 30, 2019')
+  right_join(iis_combined, by=c('fips', 'state')) %>%
+  filter(time == '2018-01-01')
 
 #
 ggplot(compare) +
-  geom_point(aes(x=pct_pcv_epic, y=pct_pcv_iis)) +
+  geom_point(aes(x=pct_pcv_epic, y=pct_pcv_iis, color=state)) +
   theme_classic()+
-  ggtitle('Comparison of IIS data with Epic Cosmos')
+  ggtitle('Comparison of IIS data with Epic Cosmos')+
+  facet_wrap(~state)+
+  ylim(40,90)+
+xlim(40,90)
